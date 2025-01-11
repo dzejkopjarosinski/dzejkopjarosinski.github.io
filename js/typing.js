dc64c42c7bfd62ffeddd1b3c1c4994339a5669eb
Jakub Jarosiński <dzejkopjarosinski@gmail.com>
2025-01-11 18:33:47 +0100

    website update

--- js/typing.js
+++ js/typing.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var typed = new Typed(".typing", {
-        strings: ["Data Scientist", "Web Developer", "Marketing Specialist", "Financial Analyst", "Researcher", "Blockchain enthusiast"],
+        strings: ["Software Developer", "Data Scientist", "Web Developer", "Researcher", "AI enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
