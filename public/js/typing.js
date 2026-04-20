92a9223b926824a39b855696f9054c45002401d7
Jacob Jarosiński <63823444+dzejkopjarosinski@users.noreply.github.com>
2026-04-20 17:48:47 +0200

    Revert "New me"

--- public/js/typing.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    var typed = new Typed(".typing", {
-        strings: ["Software Developer", "Data Scientist", "Web Developer", "Researcher", "AI enthusiast"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-});
